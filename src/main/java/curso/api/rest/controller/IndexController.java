f5725a4d29ade2dc8e8c3418056348ce2dd5d17b
Flavio Salgado <55590640+flaviosalgado2@users.noreply.github.com>
2020-06-19 09:27:55 -0300

    primeira versao
    inicial

---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,23 @@
+package curso.api.rest.controller;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class IndexController {
+
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity init(@RequestParam(value = "nome", required = true, defaultValue = "<<<Nome não informado!>>>") String nome) {
+
+		System.out.println("Parametro recebido: " + nome);
+
+		return new ResponseEntity("Usuário recebido é: " + nome, HttpStatus.OK);
+	}
+
+}
---
+++ src/test/java/com/example/cursospringrestapi/CursospringrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursospringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
