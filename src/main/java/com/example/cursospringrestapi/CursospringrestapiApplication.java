e7bfba585e15b1dadff595ef3c4aa6401ce1086b
flaviosalgado2 <flaviosalgado2@hotmail.com>
2021-01-27 22:23:16 -0300

    autorizando cros na API

--- src/main/java/com/example/cursospringrestapi/CursospringrestapiApplication.java
+++ src/main/java/com/example/cursospringrestapi/CursospringrestapiApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "curso.api.rest.model" })
@@ -18,10 +21,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class CursospringrestapiApplication {
+public class CursospringrestapiApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringrestapiApplication.class, args);
+	}
+	
+	@CrossOrigin
+	//mapeamento global que reflete em todo o sistema
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario/**")
+		.allowedMethods("*")
+		.allowedOrigins("*");
 	}
 
 }
